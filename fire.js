b1b7a940fd168bfb96412b69ea3122a01bac6dea
Firdaus Adib <i@firdaus.my>
2014-12-24 13:03:51 +0000

    First commit

---
+++ fire.js
@@ -0,0 +1,7 @@
+javascript: void((function(d) {
+    var e = d.createElement('script');
+    e.setAttribute('type', 'text/javascript');
+    e.setAttribute('charset', 'UTF-8');
+    e.setAttribute('src', '//az168019.vo.msecnd.net/scripts/degreed/media/bookmarklet.js?v=1054609229?r=' + Math.random() * 99999999);
+    d.getElementsByTagName('body')[0].appendChild(e)
+})(document));
